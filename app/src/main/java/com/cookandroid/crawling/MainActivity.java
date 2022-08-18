c268dfabefea3d0c262f25aabd109c7de1962b86
SANGYOON <qlrqod123123@naver.com>
2022-08-19 00:09:29 +0900

    AsyncTask deprecated로 인한 rxJava로 대체

--- app/src/main/java/com/cookandroid/crawling/MainActivity.java
+++ app/src/main/java/com/cookandroid/crawling/MainActivity.java
@@ -1,13 +1,8 @@
 package com.cookandroid.crawling;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -17,8 +12,14 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
     TextView tv_textView;
+    String URLs = "https://lostark.game.onstove.com/Profile/Character/0iloll0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +28,47 @@ public class MainActivity extends AppCompatActivity {
 
         tv_textView = findViewById(R.id.tv_textView);
 
-        new WeatherAsyncTask(tv_textView).execute();
+        //new WeatherAsyncTask(tv_textView).execute();
+        BackgroundTask(URLs);
+
+    }
+
+    Disposable backgroundtask;
+    void BackgroundTask(String URLs) {
+        //onPreExecute
+
+        backgroundtask = Observable.fromCallable(() -> {
+            //doInBackground
+
+            String result = "";
+
+            try {
+                Document document = Jsoup.connect(URLs).get();
+                Elements elements = document.select("span[class=profile-character-info__name]");
+
+                for (Element element : elements) {
+                    result = result + element.text() + "\n";
+                }
+
+                return result;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe((result) -> {
+                    //onPostExecute
+
+                    tv_textView.setText(result);
+
+                    backgroundtask.dispose();
+                });
     }
 }
 
+
+/*
 class WeatherAsyncTask extends AsyncTask<String, Void, String> {
     TextView textView;
 
@@ -64,3 +102,4 @@ class WeatherAsyncTask extends AsyncTask<String, Void, String> {
         textView.setText(s);
     }
 }
+ */
